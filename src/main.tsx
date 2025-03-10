f3eb9f84d5b37e27bb810b87061c7b663c91c9ce
Ryotaro Ko <pikatenor@p1kachu.net>
2025-03-10 23:35:33 +0900

    color

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider, createSystem, defaultConfig, defaultSystem } from "@chakra-ui/react"
 import { ThemeProvider } from "next-themes"
 import { Toaster } from "./components/ui/toaster"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={createSystem(defaultConfig, {
+      theme: {
+        tokens: {
+          colors: {
+            yellow: {
+              50: "#F9FFE5",
+              100: "#F0FFB8",
+              200: "#E6FF8A",
+              300: "#DCFF5C",
+              400: "#D2FF2E",
+              500: "#C8FF00",
+              600: "#A0CC00",
+              700: "#789900",
+              800: "#506600",
+              900: "#283300",
+            },
+          },
+        },
+      },
+      globalCss: {
+        html: {
+          colorPalette: "yellow",
+        },
+      },
+    })}>
       <ThemeProvider attribute="class" disableTransitionOnChange>
         <Toaster />
         <App />
